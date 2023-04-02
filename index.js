48d6ea357e60b616d6c576448a684866978a6107
Will From Blobii Studios <52068277+GitCodeBoi1654@users.noreply.github.com>
2023-04-02 15:41:16 +0800

    Created repository.
    yay this is the start of smth big.

---
+++ index.js
@@ -0,0 +1,14 @@
+var cookies = 0;
+document.getElementById("cookie").addEventListener("click", function() {
+    cookies += 1;
+
+    if (cookies == 1 ) {
+        document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
+    }
+    if (cookies > 1 ) {
+        document.getElementById("cookies-display").textContent = "You've baked " + cookies + " cookies!";
+    }
+
+
+
+});
