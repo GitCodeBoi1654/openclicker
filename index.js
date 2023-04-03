7bf42c9a64052375fe0f0256d6055b137d7ed766
Will From Blobii Studios <52068277+GitCodeBoi1654@users.noreply.github.com>
2023-04-03 15:09:12 +0800

    Complete reset todays broken changes

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var cookies = ParseInt(localStorage.getItem("cookiesBaked"));
- if (cookies == 1 ) {
-        document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
-    }
-    if (cookies > 1 ) {
-        document.getElementById("cookies-display").textContent = "You've baked " + cookies + " cookies!";
-    }
+var cookies = 0;
+
 
 document.getElementById("cookie").addEventListener("click", function() {
     cookies += 1;
-    localStorage.setItem("cookiesBaked", cookies );
+   
 
     if (cookies == 1 ) {
         document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
