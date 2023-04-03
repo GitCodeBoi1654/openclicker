a8b4e014dd6b691a652feac5a180b373ba23363f
Will From Blobii Studios <52068277+GitCodeBoi1654@users.noreply.github.com>
2023-04-03 13:40:53 +0800

    fixed string parsing bug
    :)

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var cookies = localStorage.getItem("cookiesBaked");
- if (cookies == 1 ) {
-        document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
-    }
-    if (cookies > 1 ) {
-        document.getElementById("cookies-display").textContent = "You've baked " + cookies + " cookies!";
-    }
+var cookies = parseInt(localStorage.getItem("cookiesBaked"));
+
+if (cookies == 1 ) {
+    document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
+}
+if (cookies > 1 ) {
+    document.getElementById("cookies-display").textContent = "You've baked " + cookies + " cookies!";
+}
 
 document.getElementById("cookie").addEventListener("click", function() {
     cookies += 1;
-    localStorage.setItem("cookiesBaked", cookies );
+    localStorage.setItem("cookiesBaked", cookies.toString() );
 
     if (cookies == 1 ) {
         document.getElementById("cookies-display").textContent = "You've baked 1 cookie!";
@@ -16,7 +17,4 @@ document.getElementById("cookie").addEventListener("click", function() {
     if (cookies > 1 ) {
         document.getElementById("cookies-display").textContent = "You've baked " + cookies + " cookies!";
     }
-
-
-
 });
